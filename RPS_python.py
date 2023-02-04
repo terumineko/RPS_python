405077c960e8a2352eec526084eb36d50c38176a
terumineko <77076768+terumineko@users.noreply.github.com>
2023-02-04 02:35:00 +0100

    Add files via upload

--- RPS_python.py
+++ RPS_python.py
@@ -1,14 +1,17 @@
 # variables, naming a code so that when it is called it will always do "x"
 # function, a set of codes that only runs when it is called
-# dictionaries are used to store data values and key value pairs { , : } using , to seperate each and : for setting a pair
+# dictionaries are used to store data values and key value pairs { , : } using,to separate each and : for setting a pair
 # libraries are used to access additional features without needing to write extra code
 
 import random
 
 running = True
+wins = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
+screen = {"rock": "Rock go smash.", "paper": "Scissors go snipsnip.", "scissors": "Paper go woosh."}
+
 
 def get_choices():
-    player_choices = input("Enter a choice (rock, paper, scissors): ")
+    player_choices = input("Enter a choice (rock, paper, scissors): ").lower()
     options = ["rock", "paper", "scissors"]
     computer_choices = random.choice(options)
     choices = {"player": player_choices, "computer": computer_choices}
@@ -18,26 +21,18 @@ def get_choices():
 def check_win(player, computer):
     print(f"You chose {player}, computer chose {computer}")
     if player == computer:
-        return restart()
+        return "Tie!"
         # adding .lower() so there are not issues with capitalization
-    elif player.lower() == "rock":
-        if computer == "scissors":
-            return "Rock go smash. You win!"
-    elif player.lower() == "scissors":
-        if computer == "paper":
-            return "Sciccors go snipsnip. You win!"
-        elif computer == "rock":
-            return "You lose."
-    elif player.lower() == "paper":
-        if computer == "rock":
-            return "Paper go woosh. You win!"
-        elif computer == "scissors":
-            return "You lose."
+    elif wins[player] == computer:
+        return f"{screen[player]} You win!"
+    elif wins[computer] == player:
+        return f"{screen[computer]} You lose!"
     else:
         return "Invalid choice."
 
 
 def restart():
+    global running
     restart = input("Try again?").lower()
     if restart != "yes":
         running = False
@@ -48,4 +43,3 @@ while running == True:
     results = check_win(choices["player"], choices["computer"])
     print(results)
     restart()
-
